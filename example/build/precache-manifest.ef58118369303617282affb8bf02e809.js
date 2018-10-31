cd70013d90fae32f9105f2a9c320c11092e21eb0
lipten <lipten@liptendeMacBook-Pro.local>
2018-10-31 15:22:38 +0800

    add build

---
+++ example/build/precache-manifest.ef58118369303617282affb8bf02e809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "343e0e7eb40dfc9ee2d3",
+    "url": "/static/js/main.343e0e7e.chunk.js"
+  },
+  {
+    "revision": "2269f1689e7118b0f237",
+    "url": "/static/js/1.2269f168.chunk.js"
+  },
+  {
+    "revision": "343e0e7eb40dfc9ee2d3",
+    "url": "/static/css/main.cbee2905.chunk.css"
+  },
+  {
+    "revision": "1d0f2b8bf3e7053250ff40ab141634d0",
+    "url": "/index.html"
+  }
+];
